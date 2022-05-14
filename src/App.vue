ee1b8fa6a9f5e5b4c9449816c775d8c9127ec3ab
vahidrnaderi <vahidrnaderi@gmail.com>
2022-05-14 13:15:44 +0430

    Initiate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Header />
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Sections/Header.vue'
+import Home from './components/Pages/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Home,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
